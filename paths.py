d016459b03ab820601768b8f28f1e70702f9f699
Maxi <maximilian.wess@ntnu.no>
2023-01-31 19:28:27 +0100

    alskdm

--- paths.py
+++ paths.py
@@ -1,7 +1,8 @@
-original_data_path = None # Path to the original ANHIR dataset 
-csv_path = None # Path to the ANHIR .csv file
+original_data_path = "/home/maximilw/workbench/ANHIR_MHA" # Path to the original ANHIR dataset 
+#csv_path = "/home/maximilw/workbench/datasets/anhir/dataset_medium.csv" # Path to the ANHIR .csv file
+csv_path = "/home/maximilw/workbench/ANHIR_MHA/dataset_medium.csv" # Path to the ANHIR .csv file
 
-parsed_data_path = None # Path to the ANHIR dataset (parsed, in the .mha format)
+parsed_data_path = "/home/maximilw/workbench/ANHIR_MHA/" # Path to the ANHIR dataset (parsed, in the .mha format)
 
 training_path = None # Path to the training dataset (nonrigid)
 validation_path = None # Path to the validation dataset (nonrigid)
